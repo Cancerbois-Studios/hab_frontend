14d17cc132fca41103c741e47c81fd24267ef59d
Anders Thygesen <anthy@student.sdu.dk>
2018-04-03 00:16:04 +0200

    Added bootstrap features. Made login screen more appealing. Added register button to login screen. (register site is now deprecated) CSS class btn is now form-btn to prevent overlapping with bootstrap.

--- src/app/modules/framework/components/login/login.component.ts
+++ src/app/modules/framework/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { AuthenticationService } from '../../services/authentication.service';
+import { HttpReqsService, HttpDefined } from '../../framework-export-barrel';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -13,14 +15,31 @@ export class LoginComponent implements OnInit {
   public username;
   public password;
 
-  constructor(private authenticationService: AuthenticationService) { }
+  constructor(
+    private httpReqs: HttpReqsService,
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) { }
 
   ngOnInit() {
+  }
+
+  public register() {
+    let reqOption: HttpDefined = {
+      requestResource: 'http://skjoldtoft.dk/daniel/hab/index.php',
+      data: {class: "authentication",
+      method: "register", username: this.username, password: this.password },
+      statusCode: [200]
+    };
+    this.httpReqs.sendPostRequest(reqOption).subscribe((data) => {
+      this.login();
+    });
   }
 
   public login() {
     this.authenticationService.login(this.username,this.password).subscribe(() => {
       this.loggedIn.emit(true);
+      this.router.navigate(['/home']);
     });
   }
 
