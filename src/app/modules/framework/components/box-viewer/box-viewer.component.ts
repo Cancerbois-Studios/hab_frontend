d48d349eab9877a9e10f775013e541a96c546b8e
Anders Thygesen <anthy@student.sdu.dk>
2018-04-02 21:13:55 +0200

    Added a filter that dynamically scales boxes in the box-viewer. Currently relies on a setTimeout, which doesn't seem good.

--- src/app/modules/framework/components/box-viewer/box-viewer.component.ts
+++ src/app/modules/framework/components/box-viewer/box-viewer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-box-viewer',
@@ -6,16 +6,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./box-viewer.component.css']
 })
 export class BoxViewerComponent implements OnInit {
+  @Input() showFilter?= false;
 
-  public boxes = 20;
+  public amounts: any[];
+  public amountSelected: number;
 
   constructor() { }
 
   ngOnInit() {
+    this.amounts = [6, 12, 24];
+    this.amountSelected = this.amounts[2];
   }
 
-  public addBox() {
-    this.boxes++;
+  public onViewChanges(_amount) {
+    setTimeout(() => {
+      let container = document.getElementsByClassName("grid-container")[0] as HTMLElement;
+      var elements = document.getElementsByClassName("grid-item") as HTMLCollectionOf<HTMLElement>;
+
+      switch (+_amount) {
+        case 6:
+          for (let i = 0; i < _amount; i++) {
+            elements[i].style.height = "200px";
+          }
+          container.style.setProperty("grid-template-columns", "repeat(auto-fill, minmax(200px, 1fr))");
+          break;
+        case 12:
+          for (let i = 0; i < _amount; i++) {
+            elements[i].style.height = "150px";
+          }
+          container.style.setProperty("grid-template-columns", "repeat(auto-fill, minmax(150px, 1fr))");
+          break;
+        case 24:
+          for (let i = 0; i < _amount; i++) {
+            elements[i].style.height = "100px";
+          }
+          container.style.setProperty("grid-template-columns", "repeat(auto-fill, minmax(100px, 1fr))");
+          break;
+        default:
+          break;
+      }
+    }, 0);
   }
 
 }
