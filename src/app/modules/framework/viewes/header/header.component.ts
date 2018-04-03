ab4c6392a33a92ea42587b6c9a0cea45abebd42d
ANDERS\AKT <anthy16@student.sdu.dk>
2018-04-03 13:03:54 +0200

    More responsive magic. Added a dropdown menu at certain widths.

--- src/app/modules/framework/viewes/header/header.component.ts
+++ src/app/modules/framework/viewes/header/header.component.ts
@@ -8,13 +8,21 @@ import { GlobalService } from '../../framework-export-barrel';
 })
 export class HeaderComponent implements OnInit {
 
+  public menuOpened: boolean = false;
+  public buttonRow: any[];
+
   constructor(private globalService: GlobalService) { }
 
   ngOnInit() {
+    this.buttonRow = [['CNS','/chris-norwegian-states'],['Register','/register'],['Login','/login'],['Logout','/logout'],['Home','/home']];
   }
 
   public navigateTo(url: string) {
     this.globalService.routeTo([url]);
+  }
+
+  public openMenu() {
+    this.menuOpened = !this.menuOpened;
   }
 
 }
